4bc2be01be02d05659305d1766e840c5c33f24bd
Jochen Heizmann <info@intermediaware.com>
2017-03-23 15:44:48 +0100

    cleand up default index.php

--- public/index.php
+++ public/index.php
@@ -1,22 +1,6 @@
 <?php
 
-
-
-$pdo = new PDO('mysql:host=mysql;dbname=db', 'root', '');
-
-$sql = <<<SQL
-    SELECT * FROM user;
-SQL;
-
-var_dump($pdo);
-
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo "<pre>";
-print_r($result);
-echo "</pre>";
+php_info();
 
 ?>
 
